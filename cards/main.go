e9913628136895700f9af6463047673e482072b1
Thyago Volpatto <tvolpatto@gmail.com>
2022-09-15 12:40:49 -0600

    How to: variable declaration and functions

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	// var card string = "Ace of Spades"
+	card := newCard()
+
+	fmt.Println(card)
+
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
