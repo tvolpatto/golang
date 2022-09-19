e2ec2f840a2b968932bad10008e9ad21f0c786c2
Thyago Volpatto <tvolpatto@gmail.com>
2022-09-19 11:13:47 -0600

    Testing file save and loading

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,29 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 40 {
 		t.Errorf("Expected lenght: 40, but got %d", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first element: Ace of Spades, but got %s", d[0])
+
+	}
+
+	if d[len(d)-1] != "Ten of Clubs" {
+		t.Errorf("Expected last element: Ace of Spades, but got %s", d[len(d)-1])
+
+	}
+}
+
+func TestSaveToFileAndOpenDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 40 {
+		t.Errorf("Expected lenght: 40, but got %d", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
