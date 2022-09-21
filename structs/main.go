983ff9865ad7ed706fb0fe75a444bfa36083ae38
Thyago Volpatto <tvolpatto@gmail.com>
2022-09-20 19:39:40 -0600

    How to: sctruts inside structs

--- structs/main.go
+++ structs/main.go
@@ -2,22 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 
-	john := person{"John", "Travolta"}
-	thyago := person{firstName: "Thyago", lastName: "Volpatto"}
+	//john := person{"John", "Travolta"}
+	thyago := person{
+		firstName: "Thyago",
+		lastName:  "Volpatto",
+		contact: contactInfo{
+			email:   "myemail@gmail.com",
+			zipcode: 90887,
+		},
+	}
 
 	var juca person
 	juca.firstName = "Juca"
 	juca.lastName = "Feliz"
 
 	fmt.Println(thyago)
-	fmt.Println(john)
+	//fmt.Println(john)
 	fmt.Printf("%+v", juca)
 
 }
